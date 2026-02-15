59b7882413a0453c68136c51271e1db9b768cca3
Kenji Garland <kenjigarland@gmail.com>
2026-02-15 18:25:46 +0000

    More attempts at better auto-selection

--- lib/slidermapping.lua
+++ lib/slidermapping.lua
@@ -25,7 +25,9 @@ end
 
 -- respond to an absolute movement. values must be scaled to [0, 1]
 function SliderMapping:move(old_value, new_value)
-	if (math.abs(new_value - self.value) <= 0.02 or math.abs(old_value - self.value) <= 0.02) then
+	local crossed = (old_value <= self.value and self.value <= new_value)
+	local close = (math.abs(new_value - self.value) <= 0.02) or (math.abs(old_value - self.value) <= 0.02)
+	if crossed or close then
 		self.param:set_raw(new_value)
 	end
 end
